b6dbb52c3d55c3ff4c97322978b738bede07a394
Thyago Mendes <54914480+thymendes@users.noreply.github.com>
2021-10-01 11:02:22 -0300

    Add files via upload

---
+++ ApiCatalogoJogos/Exceptions/JogoJaCadastradoException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ApiCatalogoJogos.Exceptions
+{
+    public class JogoJaCadastradoException : Exception
+    {
+        public JogoJaCadastradoException()
+            : base("Este já jogo está cadastrado")
+        { }
+    }
+}
---
+++ ApiCatalogoJogos/Exceptions/JogoNaoCadastradoException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ApiCatalogoJogos.Exceptions
+{
+    public class JogoNaoCadastradoException : Exception
+    {
+        public JogoNaoCadastradoException()
+            : base("Este jogo não está cadastrado")
+        { }
+    }
+}
